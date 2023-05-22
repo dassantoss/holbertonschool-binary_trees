fa7cd650e6349a12edcb88d3b15833a2ff136df3
Santiago E <santy.e@gmail.com>
2023-05-22 09:44:01 -0500

    Add function that goes through a binary tree using pre-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -50,4 +50,7 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 /*Function that checks if a given node is root*/
 int binary_tree_is_root(const binary_tree_t *node);
 
+/*Function that goes through a binary tree using pre order traversal*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
 #endif
