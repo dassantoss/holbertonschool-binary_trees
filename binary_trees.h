ac31276101659ac887d0bd6cf52b98a1b108d3ce
Santiago E <santy.e@gmail.com>
2023-05-23 18:31:00 -0500

    Update header with advanced prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -90,6 +90,18 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 /*function that finds the uncle of a node*/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-/*Advanced:*/
+/* Prototypes advanced */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
 
 #endif
