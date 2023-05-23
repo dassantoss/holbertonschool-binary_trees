f2198f027439955cab883ba713b3f4e113ee8bc1
Santiago E <santy.e@gmail.com>
2023-05-23 17:48:38 -0500

    Fix betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,10 +8,10 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return 1;
+		return (1);
 
-	return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
