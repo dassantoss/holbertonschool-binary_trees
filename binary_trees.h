52de8b7f6882aa4caf25fdebcda61603c89e8b24
Santiago E <santy.e@gmail.com>
2023-05-23 16:49:14 -0500

    Add function that goes through a binary tree using post-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -32,28 +32,32 @@ typedef struct binary_tree_s heap_t;
 /*Print function*/
 void binary_tree_print(const binary_tree_t *);
 
-/* Function that creates a binary tree node*/
+/*Mandatory*/
+/*0. Function that creates a binary tree node*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
-/*function that inserts a node as the left-child of another node*/
+/*1. function that inserts a node as the left-child of another node*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
-/*function that inserts a node as right-child of another node*/
+/*2. function that inserts a node as right-child of another node*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
-/*Function that deletes an entire binary tree*/
+/*3. Function that deletes an entire binary tree*/
 void binary_tree_delete(binary_tree_t *tree);
 
-/*Function that checks if a node is leaf*/
+/*4. Function that checks if a node is leaf*/
 int binary_tree_is_leaf(const binary_tree_t *node);
 
-/*Function that checks if a given node is root*/
+/*5. Function that checks if a given node is root*/
 int binary_tree_is_root(const binary_tree_t *node);
 
-/*Function that goes through a binary tree using pre order traversal*/
+/*6. Function that goes through a binary tree using pre order traversal*/
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
-/*function that goes through a binary tree using in-order traversal*/
+/*7. function that goes through a binary tree using in-order traversal*/
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/*8. function that goes through a binary tree using post-order traversal*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
