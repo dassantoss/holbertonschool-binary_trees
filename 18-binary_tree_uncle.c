dcb50d7aa60666f0bc81780571ee729eb7b43c2f
Santiago E <santy.e@gmail.com>
2023-05-23 18:26:29 -0500

    Yes... fix betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -4,7 +4,7 @@
  * binary_tree_uncle - Finds the uncle of a node
  * @node: Pointer to the node to find the uncle
  *
- * Return: Pointer to the uncle node, 
+ * Return: Pointer to the uncle node,
  * or NULL if node is NULL or has no uncle
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
