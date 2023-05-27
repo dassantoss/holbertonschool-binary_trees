d670920091e3288944c233be8fb7c3104262a4e5
Santiago E <santy.e@gmail.com>
2023-05-26 22:35:34 -0500

    Add function that inserts a value in a Binary Search Tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,15 +9,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
 
-	if (new_node == NULL)
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
 
-	return (new_node);
+	return (new);
 }
