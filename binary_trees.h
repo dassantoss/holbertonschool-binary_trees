d3c9066cd7acb804b8c9de416ce89f40c475fad8
Santiago E <santy.e@gmail.com>
2023-05-26 22:56:35 -0500

    Fix

--- binary_trees.h
+++ binary_trees.h
@@ -7,42 +7,25 @@
 
 /**
  * struct binary_tree_s - Binary tree node
+ *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
-*/
-
-typedef struct binary_tree_s
+ */
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
-/**
- * struct queue_s - Queue node structure
- * @node: Pointer to the binary tree node
- * @next: Pointer to the next node in the queue
- */
-typedef struct queue_s
-{
-    const binary_tree_t *node;
-    struct queue_s *next;
-} queue_t;
-
-
-/*Binary Search Tree*/
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
-
-/*AVL Tree*/
 typedef struct binary_tree_s avl_t;
-
-/*Max Binary Heap*/
 typedef struct binary_tree_s heap_t;
 
-/*Print function*/
 void binary_tree_print(const binary_tree_t *);
 
 /*Mandatory*/
