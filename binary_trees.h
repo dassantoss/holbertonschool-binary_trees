c8c08446fcfe6500a830347d2b92a874e83cc055
Santiago E <santy.e@gmail.com>
2023-05-26 22:42:34 -0500

    Fix

--- binary_trees.h
+++ binary_trees.h
@@ -120,5 +120,11 @@ avl_t *avl_insert(avl_t **tree, int value);
 /*Others*/
 void enqueue(queue_t **queue, const binary_tree_t *node);
 void dequeue(queue_t **queue);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
 
 #endif
